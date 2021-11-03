4751b82efd99bab57e0e86eb28196b8fe99b87eb
ArtemUstal <artsok.1@mail.ru>
2021-11-03 19:09:32 +0500

    ReactionBondUser

--- Models/Reaction.cs
+++ Models/Reaction.cs
@@ -8,5 +8,7 @@ namespace onlyarts.Models
         public int Id { get; set; }
         public int UserId { get; set; }
         public bool Type { get; set; }
+
+        public User User { get; set; }  //соединяемся с User через UserID
     }
 }
