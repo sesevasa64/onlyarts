ff07d7c6c5d6a78cdaf3a88a7e6a0efea92cabab
ArtemUstal <artsok.1@mail.ru>
2021-11-03 20:10:04 +0500

    Migration1

---
+++ Migrations/20211103150933_ContentMigr.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace onlyarts.Migrations
+{
+    public partial class ContentMigr : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ViewCount",
+                table: "Contents",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ViewCount",
+                table: "Contents");
+        }
+    }
+}
