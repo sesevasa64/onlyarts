04213ce9c03e37baf6327ce8e55e819cb604f9dc
otvc <olegerko048@gmail.com>
2021-11-04 15:35:22 +0300

    add start component

---
+++ ClientApp/src/OnlyArts.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class OnlyArts extends Component
+{
+  render () {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default OnlyArts;
--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import OnlyArts from './OnlyArts';
 import registerServiceWorker from './registerServiceWorker';
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
@@ -10,7 +10,7 @@ const rootElement = document.getElementById('root');
 
 ReactDOM.render(
   <BrowserRouter basename={baseUrl}>
-    <App />
+    <OnlyArts/>
   </BrowserRouter>,
   rootElement);
 
