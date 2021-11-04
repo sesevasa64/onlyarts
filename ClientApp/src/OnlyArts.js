706c2b62c0b2948a8cf38c6bceebb623b294c8b1
otvc <olegerko048@gmail.com>
2021-11-04 16:13:16 +0300

    ref main component

--- ClientApp/src/OnlyArts.js
+++ ClientApp/src/OnlyArts.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import './OnlyArts.css'
+import HeaderOA from './components/HeaderOA';
+import Logo from './components/Logo';
+import NavigationMenu from './components/NavigationMenu';
+import TagList from './components/TagList';
 
 class OnlyArts extends Component
 {
   render () {
     return (
-      <div>
-
+      <div className="main-box">
+          <Logo/>
+          <HeaderOA/>
+          <NavigationMenu/>
+          <TagList/>
       </div>
     );
   }
