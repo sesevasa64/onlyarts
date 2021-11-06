5306f0a6388b6769651bd7ed77e6713d1a22e977
otvc <olegerko048@gmail.com>
2021-11-06 10:20:30 +0300

    add card model

---
+++ ClientApp/src/models/ContentCard.js
@@ -0,0 +1,15 @@
+class ContentCard
+{
+  constructor(user_name, user_image, facial_image, caption, short_about, images)
+  {
+    this.user_name = user_name;
+    this.user_image = user_image;
+    this.facial_image = facial_image;
+    this.caption = caption;
+    this.short_about = short_about;
+    this.images = images; 
+  }
+
+}
+
+export default ContentCard;
