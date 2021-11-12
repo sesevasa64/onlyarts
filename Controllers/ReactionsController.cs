5cef3776172edabda9593a5437ff4fcd1f2500cf
Vsevolod <sesevasa64@gmail.com>
2021-11-12 21:20:30 +0500

    changed api route to /api/*

--- Controllers/ReactionsController.cs
+++ Controllers/ReactionsController.cs
@@ -9,7 +9,7 @@ using onlyarts.Data;
 namespace onlyarts.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class ReactionsController : RestController
     {
         private readonly OnlyartsContext _context;
