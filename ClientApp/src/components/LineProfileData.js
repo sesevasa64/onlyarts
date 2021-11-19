792a6d20ef6e222ce43261989a944136d49fb277
otvc <olegerko048@gmail.com>
2021-11-19 16:49:40 +0300

    ref components

---
+++ ClientApp/src/components/LineProfileData.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import '../OnlyArts.css'
+import './css/LineProfileData.css'
+import views from './resources/views.png';
+import likes from './resources/likes.png';
+
+function LineProfileData(props)
+{
+  return (
+    <div className="profile-line">
+      <div>
+        <p>{props.item.name}</p><br/>
+        <img src={props.item.user.linkToAvatar}/>
+        <p><Link to={`/UserPage/${props.item.user.id}`}>{props.item.user.nickname}</Link></p>
+        <div className="mini-card-about">
+          {props.item.description}
+        </div>
+        <div>
+          <img src={views}></img> {props.item.viewCount}
+          <img className="likes_button" src={likes}></img> {props.item.likesCount}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export {LineProfileData};
