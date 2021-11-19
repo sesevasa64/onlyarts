269f9c932beb494029b9572fba841a4664382b6c
ArtemUstal <artsok.1@mail.ru>
2021-11-19 23:30:33 +0500

    create Free Subscryption in subtypes

---
+++ Migrations/20211119182654_AddTypeInSubTypes.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace onlyarts.Migrations
+{
+    public partial class AddTypeInSubTypes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "SubTypes",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "SubTypes");
+        }
+    }
+}
